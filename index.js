deedcae61c85b20b95f87003bea69e89bae5e8c3
Clay Hoover <ClayHoover@Clays-MBP.attlocal.net>
2020-10-07 15:30:01 -0400

    10/7

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ let url = 'https://pokeapi.co/api/v2/pokemon/';
 const bulbasaur = document.querySelector('bulbasaur')
 const charmander = document.querySelector('charmander')
 const squirtle = document.querySelector('squirtle')
+let name = document.querySelector(".name");   //!
+let id = document.querySelector(".id");
+let type = document.querySelector(".type");
+let name2 = document.querySelector(".name2");  
+let id2 = document.querySelector(".id2");
+let type2 = document.querySelector(".type2");
 
 fetch('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png')
 .then(response => {
-    console.log(response);
+    //console.log(response);
     return response.blob();
 })
 .then(blob => {
@@ -17,7 +23,7 @@ fetch('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/
 
 fetch('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png')
 .then(response => {
-    console.log(response);
+    //console.log(response);
     return response.blob();
 })
 .then(blob => {
@@ -27,7 +33,7 @@ fetch('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/
 
 fetch('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png')
 .then(response => {
-    console.log(response);
+    //console.log(response);
     return response.blob();
 })
 .then(blob => {
@@ -48,8 +54,20 @@ const fetchPokemon = () => {
                 type: data.types.map( type => type.type.name) .join(', '),         
                 image: data.sprites.front_default,
             };
-                console.log(bulbasaur);
+              console.log(bulbasaur)
 
+            let newName = data.name;
+            newName = newName.charAt(0).toUpperCase() + newName.slice(1);
+            name.innerText = newName;
+
+            let newId = data.id;
+            id.innerHTML = newId;
+
+            let newType = data.types.map( type => type.type.name) .join(', ');
+            newType = newType.charAt(0).toUpperCase() + newType.slice(1);
+
+            //newType = newType.charAt(7).toUpperCase() + newType.slice(8);
+            type.innerText = newType;
        
         })
 };
@@ -72,7 +90,7 @@ const fetchPokemon2 = () => {
             return response.json();
         })
         .then( data => {
-            console.log(data);
+            //console.log(data);
             const charmander = {
                 name: data.name,
                 id: data.id,
@@ -80,11 +98,19 @@ const fetchPokemon2 = () => {
                 image: data.sprites.front_default,
             };
                 console.log(charmander)
+
+                // let newName = data.name;
+                // newName = newName.charAt(0).toUpperCase() + newName.slice(1);
+                // name.innerText = newName;
+    
+                // let newId = data.id;
+                // id.innerHTML = newId;
+
+              
         });
 };
 
 fetchPokemon2();
-
 
 
 const fetchPokemon3 = () => {
@@ -93,16 +119,29 @@ const fetchPokemon3 = () => {
             return response.json();
         })
         .then( data => {
-            console.log(data);
+            //console.log(data);
             const squirtle = {
                 name: data.name,
                 id: data.id,
                 type: data.types.map( type => type.type.name) .join(', '),         
                 image: data.sprites.front_default,
             };
-                console.log(squirtle)
-            
+                // var str = JSON.stringify(getSquirtle, null, 2);
+                // var mySquirtle = squirtle.stringify(json);
+                // document.getElementById('div3').innerHTML = mySquirtle;
+                console.log(squirtle);
+                // appendData(squirtle);
         });
 };
 
 fetchPokemon3();
+
+/*
+async function getSquirtle() {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data.name);
+    console.log(data.id);
+    console.log(data.type);
+}
+*/
